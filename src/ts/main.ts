2bb3b78e9b15e22185f7287975881cfabbe944e5
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2024-05-12 16:16:18 -0300

    fix: resolvido problema anterior com abordagem diferente

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,10 +1,6 @@
 const inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input");
 const textareas: NodeListOf<HTMLTextAreaElement> =
   document.querySelectorAll("textarea");
-const form: HTMLElement = document.querySelector("form") as HTMLFormElement;
-// const button: HTMLElement = document.querySelector(
-//   "button"
-// ) as HTMLButtonElement;
 
 document.addEventListener("DOMContentLoaded", () => {
   inputs.forEach((input) => {
@@ -14,9 +10,4 @@ document.addEventListener("DOMContentLoaded", () => {
   textareas.forEach((textarea) => {
     textarea.value = "";
   });
-});
-
-form.addEventListener("submit", () => {
-  window.open("https://pt.stackoverflow.com", "_self");
-  alert("asdfadf");
 });
